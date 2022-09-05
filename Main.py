e396124115cfa5a6a9f54c3100a4e492582eb84d
URK21CS3001MATHEW <109952093+URK21CS3001MATHEW@users.noreply.github.com>
2022-09-05 14:46:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,92 +1,116 @@
-class Evaluate:
- #This class validates and evaluate postfix expression.
- #Attributes:
- #   top: An integer which denotes the index of the element at the top of the stack currently.
- #   size_of_stack: An integer which represents the size of stack.
- #   stack: A List which acts as a Stack.
+from typing import Optional
 
 
-  def __init__(self, size):
-  #Inits Evaluate with top, size_of_stack and stack.
-  #  Arguments:
-  #    size_of_stack: An integer to set the size of stack.
-    self.top = -1
-    self.size_of_stack = size
-    self.stack = []
+class Node:
+    """
+    Provide necessary documentaion
+        data=store associated data
+        next=link to next node
+    """
+
+    def __init__(self, data=None, next=None):
+        """
+        Initializes the Node with the given attributes
+        """
+        self.data = data
+        self.next = next
 
 
-  def isEmpty(self):
-    #Check whether the stack is empty.
-    #Returns:
-    # True if it is empty, else returns False
-      return len(self.stack) == 0
+class LinkedList:
+    """
+    This class implements LinkedList using Node objects
+    Methods:
+        insert_at_end-inserts node with data at the end of the list
+        status-displays all elements of the list
+    Attributes:
+        self.head-contains first node of LinkedList and None if list empty
+    """
+
+    def __init__(self):
+        """
+        Initialize the head
+        """
+        self.head = None
+
+    def insert_at_end(self, data):
+        """
+        Insert node at end of the list
+        :param data: integer data that will be used to create a node
+        """
+        new = Node(data)
+        current = self.head
+        if current is None:
+            self.head = new
+        else:
+            while current.next is not None:
+                current = current.next
+            current.next = new
+
+    def status(self):
+        """
+        It prints all the elements of list.
+        """
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(elements)
 
 
-  def pop(self):
-    #Do pop operation if the stack is not empty.
-    #Returns:
-    # The data which is popped out if the stack is not empty.
-    if not self.isEmpty():
-      self.top -= 1
-      return self.stack.pop(-1)
+class Solution:
+    """
+    Provide necessary documentation
+    Class implementing the functions to add numbers in a LinkedList
     
+    """
+
+    def addTwoNumbers(self, first_list: Optional[LinkedList], second_list: Optional[LinkedList]) -> Optional[
+        LinkedList]:
+        """
+        :param first_list: Linkedlist with non-negative integers
+        :param second_list: Linkedlist with non-negative integers
+        :return: returns the sum as a linked list
+        """
+        result = self.get_num(first_list) + self.get_num(second_list)
+        sum_list = LinkedList()
+        for digit in list(map(int, str(result)[::-1])):
+            sum_list.insert_at_end(digit)
+        return sum_list
+
+    def get_num(self, l: Optional[LinkedList]) -> int:
+        """
+        :param l: LinkedList with non-negative integers
+        :return: returns digits of the list as a single integer
+        """
+        curr = l.head
+        if curr is None:
+            return 0
+        num = ""
+        while curr is not None:
+            num = str(curr.data) + num
+            curr = curr.next
+        return int(num)
 
 
-  def push(self, operand):
-   #Push the operand to stack if the stack is not full.
-   #Arguments:
-   # operand: The operand to be pushed.
-    if len(self.stack) < self.size_of_stack:
-      self.top += 1
-      self.stack.append(operand)
-      
-
-
-  def validate_postfix_expression(self, expression):
-    #Check whether the expression is a valid postfix expression.
-    #Arguments:
-    #  expression: A list which represents the expression to be validated.
-    #Returns:
-    #  True if the expression is valid, else returns False
-    operands = [element for element in expression if element.isdigit()]
-    operators = [element for element in expression  if element in ["+", "-", "*", "/", "^"]]
-    if (len(operands) + len(operators)) == len(expression) and len(operands) == len(operators) + 1:
-        return expression[0] not in operators and expression[1] not in operators
-
-
-  def evaluate_postfix_expression(self, expression):
-    #Evaluate the postfix expression
-    #Arguments:
-    #    expression: A list which represents the the expression to be evaluated
-    #Returns:
-    #    The result of evaluated postfix expression.
-    self.stack = []
-    for element in expression:
-      if element.isdigit():
-        self.push(int(element))
-      elif element in ["+", "-", "*", "/", "^"]:
-        if element == "+":
-          result = self.stack[-2] + self.stack[-1]
-        elif element == "-":
-          result = self.stack[-2] - self.stack[-1]
-        elif element == "*":
-          result = self.stack[-2] * self.stack[-1]
-        elif element == "/":
-          result = self.stack[-2] // self.stack[-1]
-        elif element == "^":
-          result = self.stack[-2] ** self.stack[-1]
-        self.pop()
-        self.pop()
-        self.push(result)
-    return self.pop()
-    
-
-
-# Do not change the following code
-postfix_expression = input()  # Read postfix expression
-tokens = postfix_expression.split()
-evaluate = Evaluate(len(tokens))
-if evaluate.validate_postfix_expression(tokens):
-    print(evaluate.evaluate_postfix_expression(tokens))
-else:
-    print('Invalid postfix expression')
+# Do not edit the following code
+# Create an instance for LinkedList
+first_list = LinkedList()
+# Create an another instance for LinkedListT
+second_list = LinkedList()
+# Read data for first list
+data_for_first_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of first_list
+for data in data_for_first_list:
+    first_list.insert_at_end(data)
+# Read data for second list
+data_for_second_list = list(map(int, input().strip().split(" ")))
+# Add data at the end of second_list
+for data in data_for_second_list:
+    second_list.insert_at_end(data)
+# Create an instance for Solution
+solution = Solution()
+# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
+new_list = solution.addTwoNumbers(first_list, second_list)
+# Display the status of new_list
+new_list.status()
